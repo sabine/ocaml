95f13dc7c7dfe2295ecb0ef4ec0c7317528c798d
Gabriel Scherer <gabriel.scherer@gmail.com>
2022-02-06 23:01:03 +0100

    move generic_table functions from minor_gc.c to generic_table.c

--- runtime/minor_gc.c
+++ runtime/minor_gc.c
@@ -46,25 +46,6 @@ static atomic_intnat domains_finished_minor_gc;
 
 static atomic_uintnat caml_minor_cycles_started = 0;
 
-/* [sz] and [rsv] are numbers of entries */
-void alloc_generic_table (struct generic_table *tbl, asize_t sz,
-                                 asize_t rsv, asize_t element_size)
-{
-  void *new_table;
-
-  tbl->size = sz;
-  tbl->reserve = rsv;
-  new_table = (void *) caml_stat_alloc_noexc((tbl->size + tbl->reserve) *
-                                             element_size);
-  if (new_table == NULL) caml_fatal_error ("not enough memory");
-  if (tbl->base != NULL) caml_stat_free (tbl->base);
-  tbl->base = new_table;
-  tbl->ptr = tbl->base;
-  tbl->threshold = tbl->base + tbl->size * element_size;
-  tbl->limit = tbl->threshold;
-  tbl->end = tbl->base + (tbl->size + tbl->reserve) * element_size;
-}
-
 void caml_alloc_table (struct caml_ref_table *tbl, asize_t sz, asize_t rsv)
 {
   alloc_generic_table ((struct generic_table *) tbl, sz, rsv, sizeof (value *));
@@ -800,41 +781,6 @@ CAMLexport value caml_check_urgent_gc (value extra_root)
     CAMLdrop;
   }
   return extra_root;
-}
-
-void realloc_generic_table
-(struct generic_table *tbl, asize_t element_size,
- ev_gc_counter ev_counter_name,
- char *msg_threshold, char *msg_growing, char *msg_error)
-{
-  CAMLassert (tbl->ptr == tbl->limit);
-  CAMLassert (tbl->limit <= tbl->end);
-  CAMLassert (tbl->limit >= tbl->threshold);
-
-  if (tbl->base == NULL){
-    alloc_generic_table (tbl, Caml_state->minor_heap_wsz / 8, 256,
-                         element_size);
-  }else if (tbl->limit == tbl->threshold){
-    CAML_EV_COUNTER (ev_counter_name, 1);
-    caml_gc_message (0x08, msg_threshold, 0);
-    tbl->limit = tbl->end;
-    caml_request_minor_gc ();
-  }else{
-    asize_t sz;
-    asize_t cur_ptr = tbl->ptr - tbl->base;
-
-    tbl->size *= 2;
-    sz = (tbl->size + tbl->reserve) * element_size;
-    caml_gc_message (0x08, msg_growing, (intnat) sz/1024);
-    tbl->base = caml_stat_resize_noexc (tbl->base, sz);
-    if (tbl->base == NULL){
-      caml_fatal_error ("%s", msg_error);
-    }
-    tbl->end = tbl->base + (tbl->size + tbl->reserve) * element_size;
-    tbl->threshold = tbl->base + tbl->size * element_size;
-    tbl->ptr = tbl->base + cur_ptr;
-    tbl->limit = tbl->end;
-  }
 }
 
 void caml_realloc_ref_table (struct caml_ref_table *tbl)
